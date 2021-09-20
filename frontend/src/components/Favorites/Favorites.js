6aea4048c6a981fec5be83d56dd560d51ecb5573
Makapenko <makap88@gmail.com>
2021-09-20 12:33:22 +0300

    css filter

--- frontend/src/components/Favorites/Favorites.js
+++ frontend/src/components/Favorites/Favorites.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 
 function Favorites(props) {
   return (
     <div>
-      Favorites
+       <Link to="/addEventAndPlace">
+          <li className="margin-1 padding-1">Добавить Событие</li>
+        </Link>
     </div>
   );
 }
