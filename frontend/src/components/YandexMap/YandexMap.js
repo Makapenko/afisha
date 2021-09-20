f287149332c6926818818ead14446f911e961809
Kurlatov <kurlatovd@gmail.com>
2021-09-20 10:08:06 +0300

    In progress

--- frontend/src/components/YandexMap/YandexMap.js
+++ frontend/src/components/YandexMap/YandexMap.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import { YMaps, Map, Placemark, ZoomControl, Clusterer } from 'react-yandex-maps';
-import { useState } from 'react';
-import points from './points.json';
+import React from "react";
+import {
+  YMaps,
+  Map,
+  Placemark,
+  ZoomControl,
+  Clusterer,
+} from "react-yandex-maps";
+import { useState } from "react";
+import points from "./points";
+// import myIcon from "../iconsForYMaps/bars.png";
+import myIcon2 from "../iconsForYMaps/quests.png";
 
 function YandexMap() {
   const [lat, setLat] = useState(0);
@@ -9,64 +17,78 @@ function YandexMap() {
 
   const mapState = {
     center: [lat, lng],
-    zoom: 13
-  }
+    zoom: 13,
+  };
 
-  const getPointData = index => {
+  const getPointData = (index) => {
     return {
       balloonContentBody: "placemark <strong>balloon " + index + "</strong>",
-      clusterCaption: "placemark <strong>" + index + "</strong>"
+      clusterCaption: "placemark <strong>" + index + "</strong>",
     };
   };
 
   const getPointOptions = () => {
     return {
-      preset: "islands#violetIcon"
+      preset: "islands#violetIcon",
+      iconImageHref: myIcon2,
+      iconImageSize: [25, 30],
+      iconLayout: "default#image",
     };
   };
 
-  if ('geolocation' in navigator) {
-    console.log('Geolocation available');
-    navigator.geolocation.getCurrentPosition(position => {
-      setLat(position.coords.latitude)
-      setLng(position.coords.longitude)
+  if ("geolocation" in navigator) {
+    console.log("Geolocation available");
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLng(position.coords.longitude);
     });
   } else {
-    console.log('Geolocation not available');
+    console.log("Geolocation not available");
   }
 
   return (
     <>
-    <div>
-    <p> Latitude: <span id='latitude'>{lat}</span><br /> 
-    Longitude: <span id='longitude'>{lng}</span></p>
-    </div>
-    <YMaps>
       <div>
-      <Map state={mapState} >
-      <ZoomControl options={{ float: 'left' }} />
-      <Placemark  geometry={[lat, lng]} />
-      <Clusterer
-            options={{
-              preset: "islands#invertedVioletClusterIcons",
-              groupByCoordinates: false,
-              clusterDisableClickZoom: true,
-              clusterHideIconOnBalloonOpen: false,
-              geoObjectHideIconOnBalloonOpen: false
-            }}
-          >
-          {points.map((coordinates, idx) => (
-              <Placemark
-                key={idx}
-                geometry={ coordinates }
-                properties={getPointData(idx)}
-                options={getPointOptions()}
-              />
-            ))}
-          </Clusterer>
-      </Map>
+        <p>
+          {" "}
+          Latitude: <span id="latitude">{lat}</span>
+          <br />
+          Longitude: <span id="longitude">{lng}</span>
+        </p>
       </div>
-    </YMaps>
+      <YMaps>
+        <div>
+          <Map state={mapState}>
+            <ZoomControl options={{ float: "left" }} />
+            <Placemark
+              geometry={[lat, lng]}
+              options={{
+                // iconLayout: "default#image",
+                iconImageHref: 'islands#violetRunIcon',
+                iconImageSize: [25, 30],
+              }}
+            />
+            <Clusterer
+              options={{
+                preset: "islands#invertedVioletClusterIcons",
+                groupByCoordinates: false,
+                clusterDisableClickZoom: true,
+                clusterHideIconOnBalloonOpen: false,
+                geoObjectHideIconOnBalloonOpen: false,
+              }}
+            >
+              {points.map((coordinates, idx) => (
+                <Placemark
+                  key={idx}
+                  geometry={coordinates}
+                  properties={getPointData(idx)}
+                  options={getPointOptions()}
+                />
+              ))}
+            </Clusterer>
+          </Map>
+        </div>
+      </YMaps>
     </>
   );
 }
