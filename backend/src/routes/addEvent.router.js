1c70b13926e3c5161230b3d950be115119ef2f9a
kirill petrov <fantaskiras@gmail.com>
2021-09-24 13:40:33 +0300

    good branch

--- backend/src/routes/addEvent.router.js
+++ backend/src/routes/addEvent.router.js
@@ -42,56 +42,6 @@ router.route('/')
         code: 'EVENT SAVED',
         message: 'Событие добавлено в БД',
       });
-    // let newEvent = null;
-
-    // try {
-    //   newEvent = await db.Event.create({
-    //     title: body.title,
-    //     description: body.description,
-    //     AdminId: body.AdminId,
-    //     LocationId: Number(body.LocationId),
-    //     price: body.price,
-    //     subcategory: body.subcategory,
-    //     timeStart: body.startTime,
-    //     timeEnd: body.endTime,
-    //     dorsOpen: body.doorsOpen,
-    //     dateStart: body.startDate,
-    //     dateEnd: body.endDate,
-    //     linkToRegister: body.linkToRe,
-    //     linkToBuy: body.linkToBuy,
-    //     linkToEvent: body.linkToEvent,
-    //   });
-
-    //   res
-    //     .status(200)
-    //     .json({
-    //       code: 'EVENT SAVED',
-    //       message: 'Событие добавлено в БД',
-    //     });
-    // } catch (error) {
-    //   // eslint-disable-next-line no-console
-    //   console.log(error.message);
-
-    //   res
-    //     .status(500)
-    //     .json({
-    //       message: 'Ошибка записи в БД',
-    //     });
-    // }
-
-    // body.url.forEach(async (link) => {
-    //   try {
-    //     if (link !== '') {
-    //       await db.EventPhoto.create({
-    //         EventId: newEvent.id,
-    //         url: link,
-    //       });
-    //     }
-    //   } catch (error) {
-    //     // eslint-disable-next-line no-console
-    //     console.log(error.message);
-    //   }
-    // });
   });
 
 module.exports = router;
