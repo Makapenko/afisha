d32abec5eb2a3db23f7d43782a883d668f7b3a0d
Fedor <fedrdrdr@yandex.ru>
2021-09-24 11:07:34 +0300

    add buttons

--- frontend/src/components/CardList/Events.js
+++ frontend/src/components/CardList/Events.js
@@ -1,14 +1,14 @@
 
 
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
+import calendar from '../../icons/filters/calendar.png';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Event from './Event';
 import style from './Events.module.css';
 
 // для кнопок навигации
-import { PUSH_BUTOON } from '../../redux/actionTypes';  
+import { PUSH_BUTOON } from '../../redux/actionTypes';
 
 function CardList(props) {
   const dispatch = useDispatch();
@@ -28,138 +28,65 @@ function CardList(props) {
   const [value, setValue] = useState(true)
   const [value1, setValue1] = useState(true)
   const [isToday, setIsToday] = useState(true)
-function isFree(){
-  // if(value&&isToday){
-    if(value){
-     
-    // console.log("бесплатно и всегда", events);
-    setValue(false)
-    dispatch({
-      type:"IS_FREE_EVENT",
-      payload:events
-    })
-  //   setCopyEvents(events.filter(function (el) {
-  //   return hasNumbers(el.price)!==true
-  // })
-  // )
-}else if(!value){
-    // console.log(" любые  всегда", events);
-    setValue(true)
-    dispatch({
-      type:"ALL_EVENT",
-      payload: copyEvents
-    })
-    // setCopyEvents(events)
+  function isFree() {
+    // if(value&&isToday){
+    if (value) {
+
+      // console.log("бесплатно и всегда", events);
+      setValue(false)
+      dispatch({
+        type: "IS_FREE_EVENT",
+        payload: events
+      })
+      //   setCopyEvents(events.filter(function (el) {
+      //   return hasNumbers(el.price)!==true
+      // })
+      // )
+    } else if (!value) {
+      // console.log(" любые  всегда", events);
+      setValue(true)
+      dispatch({
+        type: "ALL_EVENT",
+        payload: copyEvents
+      })
+      // setCopyEvents(events)
+    }
+   
   }
-  // if(value&&!isToday){
-  //   console.log("бесплатно и сегодня");
-  //   setValue(false)
-  //   setCopyEvents(events.filter(function (el) {
-  //   return hasNumbers(el.price)!==true
-  // }))
-  // }
-  // if(!value&&isToday){
-    
-  // if(!value&&!isToday){
-  //   console.log(" любые  всегда");
-  //   setValue(true)
+ 
+  function today() {
 
+    if (value1) {
 
-  // }
-}
-// function Today(){
-//   var todaysDate = new Date();
-//   if(value&&isToday){
-//     console.log("бесплатно и всегда");
-//     setValue(false)
-//     setCopyEvents(events.filter(function (el) {
-//     return hasNumbers(el.price)!==true
-//   }))
-//   if(value&&!isToday){
-//     console.log("бесплатно и сегодня");
-//     setValue(false)
-//     setCopyEvents(events.filter(function (el) {
-//     return hasNumbers(el.price)!==true
-//   }))
-//   }
-//   if(!value&&isToday){
-//     console.log(" любые  всегда");
+      setValue1(false)
+      dispatch({
+        type: "IS_TODAY",
+        payload: events
+      })
 
-//     setValue(true)
+    } else if (!value1) {
+      setValue1(true)
+      dispatch({
+        type: "ALL_EVENT_IS_NOT_TODAY",
+        payload: copyEvents
+      })
 
-//     setCopyEvents(events)
-//   }
-//   if(!value&&!isToday){
-//     console.log(" любые  всегда");
-
-  // }
-  // if(value){
-  //   console.log("сегодня и бесплатные и платные");
-  //   setIsToday(false)
-  //   setCopyEvents(copyEvents.filter(function (el) {
-  //     let inputDate = new Date(el.dateStart);
-  //     console.log(inputDate);
-  //     return inputDate.setHours(0,0,0,0)==todaysDate.setHours(0,0,0,0)
-  //   }))
-  // }else{
-  //   setIsToday(true)
-  //   console.log("сегодня и бесплатные");
-
-  //   setCopyEvents()
-//   }
-  
-// }
-// function hasNumbers(t)
-// {
-// var regex = /\d/g;
-// return regex.test(t);
-// }  
-// function isFree(){
-//   // if(value&&isToday){
-//     if(value){
-     
-//     console.log("бесплатно и всегда", events);
-//     setValue(false)
-//     dispatch({
-//       type:"IS_FREE_EVENT",
-//       payload:events
-//     })
-//   //   setCopyEvents(events.filter(function (el) {
-//   //   return hasNumbers(el.price)!==true
-//   // })
-//   // )
-// }else if(!value){
-//     console.log(" любые  всегда", events);
-//     setValue(true)
-//     dispatch({
-//       type:"ALL_EVENT",
-//       payload: copyEvents
-//     })
-//     // setCopyEvents(events)
-//   }
-function today(){
-  
-  if(value1){
-     
-    setValue1(false)
-    dispatch({
-      type:"IS_TODAY",
-      payload:events
-    })
-
-}else if(!value1){
-  setValue1(true)
-  dispatch({
-    type:"ALL_EVENT_IS_NOT_TODAY",
-    payload: copyEvents
-  })
-
-}
-}
+    }
+  }
   return (
     <>
-    <div onClick={isFree} >бесплатно</div> 
-    <div onClick={today} >Сегодня</div>
+      <div className={style.header}>
+        {!value
+          ?<div onClick={isFree} className={style.active} >Бесплатно</div>
+          :<div onClick={isFree} >Бесплатно</div>
+        }
+        {!value1
+          ?<div onClick={today} className={style.active} >Сегодня</div>
+          :<div onClick={today} >Сегодня</div>
+        }
+        {/* <div onClick={today} >Сегодня</div> */}
+        <img src={calendar} alt="" height='20vh' />
+      </div>
 
       {events
         ? <div className={style.cardlist}>{events.map(event =>
